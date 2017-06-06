98f641353db9f6d9e6dc83edee3f20576f314999
Joe HurowitzE <joehurowitz@gmail.com>
2017-06-05 23:49:31 -0400

    fixing hangman

--- assets/game1.js
+++ assets/game1.js
@@ -5,19 +5,7 @@ var word= "";
 var answerArray = [];
 
 
-document.onkeyup = function(event){
-	var letter = String.fromCharCode(event.keyCode).toLowerCase();
 
-	if (letter === "SP"){
-		init();
-	}
-	else if (letter !== "SP"){
-		showThisMessage = "Please press spacebar to initialize";
-	}
-	
-		guessOne();
-	
-} //close event
 
 function init(){
 word = library[Math.floor(Math.random()*library.length)];
@@ -28,61 +16,79 @@ for(var i=0;i<word.length;i++){
 	answerArray[i] = "_";
 }
 document.getElementById("answer").innerHTML=answerArray.join(" ");
-document.getElementById("message").innerHTML= "Type a letter then press guess or quit to stop playing";
+document.getElementById("message").innerHTML= "Type a letter or quit to stop playing";
 
-} //close init()
+}; //close init()
 
 function guessOne(){
 
 	var guess = event.key;
 	var showThisMessage = "";
 	var lettersAlreadyGuessed =[];
+	var losses=0;
+	var wins=0;
 	showThisMessage = "Please enter another letter : ";
+
 	if(guess.length !==1){
 		showThisMessage = "Please enter only one letter";
 	}
+
 	else {
 		for (var i=0;i<word.length;i++){
 			if(word[i] === guess){
+				lettersAlreadyGuessed.push(guess);
 				answerArray[i] = guess;
 				showThisMessage = "Yes! " + guess + " is in the answer";
+				
 			}
 		
 		}
 
 	var remaining_letters = answerArray.length;
-	for(var i=0;i<answerArray.length;i++){
+
+	for(var i=0;i<answerArray.length;i++){ 
 		if(answerArray[i] !== "_"){
 			remaining_letters -= 1;
+			}
+		} 
+
+		if (remaining_letters === 0){
+			showThisMessage = "Yes , you guessed the word !";
+			
 		}
-	} 
 
-	if (remaining_letters === 0){
-		showThisMessage = "Yes , you guessed the word !";
-	}
+		if (showThisMessage === ""){
+			lettersAlreadyGuessed.push(guess);
+			showThisMessage = "Sorry, no " + guess + " in the word";
+		}
+		
+		if(lettersAlreadyGuessed.length >10){
+		showThisMessage = "Sorry you lost this game";
+		losses +=1;
+		quit();
 
-	if (showThisMessage === ""){
-		showThisMessage = "Sorry, no " + guess + " in the word";
-	}
+		}
 
 
-}// else close
+
+
+	}// else close
 	
-	//document.getElementById("message1").innerHTML = lettersAlreadyGuessed;
 	
 	document.getElementById("answer").innerHTML = answerArray.join(" ");
 	document.getElementById("message").innerHTML = showThisMessage;
-	} //close guesOne(); 
+	}; //close guesOne(); 
 
 
 function quit(){
-	if(word.length === answerArray.length){
 	showThisMessage = "Thanks for playing ! ";
+	document.getElementById("text-box").value = " ";
 	init();
-	}
-	document.getElementById("message").innerHTML = showThisMessage;
 	
-	}
+
+	//document.getElementById("message").innerHTML = showThisMessage;
+	
+	};
 
 
 
